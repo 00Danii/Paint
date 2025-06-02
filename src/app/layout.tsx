ddfcc54448dfc66c35ec6bcad7c290e1e4b3eac1
00Danii <jdaniel.om97@gmail.com>
2025-06-02 00:45:32 -0600

    edicion de metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,24 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export const metadata: Metadata = {
-  title: "Paint",
-  description: "Hola esto es paint",
+  title: "Pizarrón",
+  description: "Hola esto es un pizarrón ",
+  openGraph: {
+    title: "Pizarrón",
+    description: "Hola esto es un pizarrón",
+    url: "",
+    siteName: "00Danii",
+    images: [
+      {
+        url: "https://i.imgur.com/otIw9Xm.png",
+        width: 1200,
+        height: 630,
+        alt: "Vista previa del pizarrón",
+      },
+    ],
+    locale: "es_MX",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
